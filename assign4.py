a829198da16095c433f45d6b63c3b68804dd2e3d
lfernandez55 <lfernandez@weber.edu>
2020-11-12 12:41:34 -0700

    Commit

---
+++ assign4.py
@@ -0,0 +1,22 @@
+# TODO: From a text file read in the text introduction of your game and print it to the console.
+# TODO: Setup a dictionary with several key value pairs. It should contain an inventory list and the initial location. Call the dictionary `player`.
+# TODO: Set up a dictionary of rooms each room having a description, a list of items and a dictionary of exits. Add 5 rooms. 
+
+# TODO: Setup on infinite loop.
+
+    # TODO: Create a variable and set it equal to the initial room stored in the `player` dictionary.
+    # TODO: Create a variable to store the command that is input by the user. Allow for commands like `take sword`
+    #       **Hint: `split()` and store the different parts in their own variables.**
+    # TODO: Create four `if` statements
+    # TODO: One that checks verbs and exits.
+    #       Make sure you check if the player can go in the direction that the have asked for.
+    #       This is where you list all of the items in the room.
+    # TODO: One that allows the user to print the current inventory.
+    # TODO: One that allows the user to quit.
+    # TODO: One that allows the player to pick up single items and odd them to the inventory of the `player`.
+    #       Also allow the user to add all items in the room to the inventory.
+
+# Required Additions:
+# TODO: In one room add a monster and have the player fight the monster (add an attack verb). Store the monster stats in a dictionary.
+# TODO: In one room have the player talk with another character save the dialog.
+# Create a map verb that prints a map of the dungeon. 
